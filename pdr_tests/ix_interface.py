6dace2ef0385b5400afb1ec62d11c54e0f695efa
m-stclair <mstclair@millionconcepts.com>
2022-02-28 21:59:06 -0500

    index_directory prototype

--- pdr_tests/ix_interface.py
+++ pdr_tests/ix_interface.py
@@ -5,7 +5,7 @@ from pdr_tests.datasets import (
     TestHasher,
 )
 
-COMMANDS = ["sort", "pick", "index", "download", "check"]
+COMMANDS = ["sort", "pick", "index", "download", "check", "index_directory"]
 
 
 def sort(dataset, product_type=None):
@@ -42,6 +42,34 @@ def check(
 ):
     hasher = TestHasher(dataset)
     hasher.hash_product_type(product_type, dump_browse, dump_kwargs)
+
+
+def index_directory(
+    target,
+    manifest,
+    output="index.csv",
+    debug=False
+):
+    from pathlib import Path
+    import pandas as pd
+    from pyarrow import parquet
+    from pdr_tests.utilz.test_utilz import console_and_log, get_product_row
+    product_rows = []
+    for file in Path(target).iterdir():
+        try:
+            match = parquet.read_table(
+                manifest, filters=[("filename", "=", file.name)]
+            )
+            assert len(match) == 1
+            url = match['url'].to_numpy()[0]
+            product_rows.append(get_product_row(file, url))
+        except KeyboardInterrupt:
+            raise
+        except Exception as ex:
+            if debug is True:
+                raise ex
+            console_and_log(f"failed on {file.name}: {type(ex)}: {ex}")
+    pd.DataFrame(product_rows).to_csv(output, index=None)
 
 
 def ix_help(*_, **__):
