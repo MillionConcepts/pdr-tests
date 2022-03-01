fca3372ffd70c6939d71b6276fd8fd61ad8172d3
m-stclair <mstclair@millionconcepts.com>
2022-03-01 13:30:35 -0500

    clean up interface staging

--- pdr_tests/ix_interface.py
+++ pdr_tests/ix_interface.py
@@ -79,35 +79,5 @@ def index_directory(
     pd.DataFrame(product_rows).to_csv(output, index=None)
 
 
-def index_directory(
-    target,
-    manifest,
-    output="index.csv",
-    debug=False
-):
-    from pathlib import Path
-    import pandas as pd
-    from pyarrow import parquet
-    from pdr_tests.datasets import assemble_urls
-    from pdr_tests.utilz.test_utilz import console_and_log, get_product_row
-    product_rows = []
-    for file in Path(target).iterdir():
-        try:
-            match = parquet.read_table(
-                manifest, filters=[("filename", "=", file.name)]
-            )
-            assert len(match) == 1, f"{file.name} not found in manifest"
-            match = match.to_pandas().iloc[0]
-            row = get_product_row(file, assemble_urls(match))
-            product_rows.append(row)
-        except KeyboardInterrupt:
-            raise
-        except Exception as ex:
-            if debug is True:
-                raise ex
-            console_and_log(f"failed on {file.name}: {type(ex)}: {ex}")
-    pd.DataFrame(product_rows).to_csv(output, index=None)
-
-
 def ix_help(*_, **__):
     print(f"Usage: valid subcommands are: {', '.join(COMMANDS)}.")
