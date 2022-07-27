6ae95481467b385de1e43e7292bcdcc14ff43f1c
Sierra-MC <sierra@millionconcepts.com>
2022-07-27 07:54:15 -0400

    accoounted for zero indexing in integer choice

--- pdr_tests/datasets.py
+++ pdr_tests/datasets.py
@@ -481,7 +481,7 @@ class S3Uploader(DatasetDefinition):
         with open(self.index_path(product_type)) as index_f, open(self.test_path(product_type), 'w+') as test_f:
             index_length = sum(1 for _ in index_f)
             print(f'index_length is {index_length}.')
-            integer_choice = np.random.choice(np.arange(1, index_length))
+            integer_choice = np.random.choice(np.arange(1, index_length-1))
             print(f'integer_choice is {integer_choice}.')
             index_f.seek(0)
             for pos, line in enumerate(index_f):
