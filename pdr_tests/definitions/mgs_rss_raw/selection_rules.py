150fdd4a01a6111436b4d6a31f0bd6753a07dd4f
curtiss9 <98858647+curtiss9@users.noreply.github.com>
2023-01-05 16:34:46 -0500

    Update mgs_rss_raw selection rules and add 3 test indices

--- pdr_tests/definitions/mgs_rss_raw/selection_rules.py
+++ pdr_tests/definitions/mgs_rss_raw/selection_rules.py
@@ -40,80 +40,74 @@ file_information = {
     "ech": {
         "manifest": MANIFEST_FILE,
         "fn_must_contain": ['csv', '.ech'],
-        "url_regex": [r'-rss-1-cru-|-rss-1-moi-|-rss-1-map-|-rss-1-ext-'],
+        "url_must_contain": ['mgs-m-rss-1-'],
         "label": "D",
     },
     # engineering channels summary data; well-labeled fixed-length tables
     "ecs": {
         "manifest": MANIFEST_FILE,
         "fn_must_contain": ['.ecs'],
-        "url_regex": [r'-rss-1-cru-|-rss-1-moi-|-rss-1-map-|-rss-1-ext-'],
+        "url_must_contain": ['mgs-m-rss-1-'],
         "label": "D",
     },
     # engineering telemetry data as function of time; well-labeled fixed-length tables
     "ect": {
         "manifest": MANIFEST_FILE,
         "fn_must_contain": ['.ect'],
-        "url_regex": [r'-rss-1-cru-|-rss-1-moi-|-rss-1-map-|-rss-1-ext-'],
+        "url_must_contain": ['mgs-m-rss-1-'],
         "label": "D",
     },
     # filtered body rates; well-labeled fixed-length tables
     "fbr": {
         "manifest": MANIFEST_FILE,
         "fn_must_contain": ['.fbr'],
-        "url_regex": [r'-rss-1-cru-|-rss-1-moi-|-rss-1-map-|-rss-1-ext-'],
+        "url_must_contain": ['mgs-m-rss-1-'],
         "label": "D",
     },
     # DSN monitor data; well-labeled fixed-length tables
     "mch": {
         "manifest": MANIFEST_FILE,
         "fn_must_contain": ['csv', '.mch'],
-        "url_regex": [r'-rss-1-cru-|-rss-1-moi-|-rss-1-map-|-rss-1-ext-'],
+        "url_must_contain": ['mgs-m-rss-1-'],
         "label": "D",
     },
     # DSN monitor data as function of time; well-labeled fixed-length tables
     "mct": {
         "manifest": MANIFEST_FILE,
         "fn_must_contain": ['.mct'],
-        "url_regex": [r'-rss-1-cru-|-rss-1-moi-|-rss-1-map-|-rss-1-ext-'],
+        "url_must_contain": ['mgs-m-rss-1-'],
         "label": "D",
     },
     # radio science receiver standard format data units; well-labeled fixed-length tables
     "rsr": {
         "manifest": MANIFEST_FILE,
         "fn_must_contain": ['.rsr'],
-        "url_regex": [r'-rss-1-cru-|-rss-1-moi-|-rss-1-map-|-rss-1-ext-'],
+        "url_must_contain": ['mgs-m-rss-1-'],
         "label": "D",
     },
-
-    
-}
-
-"""
-
+        
     # orbit data files; fixed-length tables
-    # problem tables: 1B start dates are weird, 3B are nonsense after the first row, final table (7B or 8B) should be all 0 but sometimes has random values in first couple rows.
     "odf": {
         "manifest": MANIFEST_FILE,
         "fn_must_contain": ['.odf'],
-        "url_regex": [r'-rss-1-cru-|-rss-1-moi-|-rss-1-map-|-rss-1-ext-'],
+        "url_must_contain": ['mgs-m-rss-1-'],
         "label": "D",
     },
     # original data records; fixed-length tables
-    # does not open
+    # include 'BCD' and 'Binary Coded Decimal' DATA_TYPES
     "odr": {
         "manifest": MANIFEST_FILE,
         "fn_must_contain": ['.odr'],
-        "url_regex": [r'-rss-1-cru-|-rss-1-moi-|-rss-1-map-|-rss-1-ext-'],
+        "url_must_contain": ['mgs-m-rss-1-'],
         "label": "D",
     },
     # tracking data file; fixed-length tables
-    # an error is introduced several columns (18?) into table TDF5 and propagates through the rest of the table
     "tdf": {
         "manifest": MANIFEST_FILE,
         "fn_must_contain": ['.tdf'],
-        "url_regex": [r'-rss-1-cru-|-rss-1-moi-|-rss-1-map-|-rss-1-ext-'],
+        "url_must_contain": ['mgs-m-rss-1-'],
         "label": "D",
     },
-
-"""
+    
+    
+}
