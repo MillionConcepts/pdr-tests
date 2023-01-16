fac39991b48efe8c9e08977e42c8ef4a85a16bd1
curtiss9 <98858647+curtiss9@users.noreply.github.com>
2023-01-16 12:50:18 -0500

    Add cassini_inms selection rules and test indices

---
+++ pdr_tests/definitions/cassini_inms/selection_rules.py
@@ -0,0 +1,51 @@
+"""
+This is a dictionary of information about each product type.
+the keys of the dictionary are the names of each product type. This will
+translate into local file and/or directory names for various outputs of
+pdr-tests: index files, data downloads, etc.
+
+These selection rules are ideally for the _largest individual file_ of each
+product (which in some cases will be the only file; lucky you, if so).
+
+manifest: the .parquet file that contains urls, file sizes, etc., scraped directly
+from the hosting institution for this product type. for some data sets, all
+urls will be found in the same .parquet file; for others, they may be split
+between nodes or scraping sessions.
+
+fn_must_contain: a list of strings that must be in the file name (not counting
+directories, domains, etc.) to differentiate it from the other types in the
+manifest file
+
+url_must_contain: an optional additional list of strings that must be in the
+url (counting the entire url) to differentiate this from other types in the
+manifest file. useful for specifying directories.
+
+label: "A" if the labels for this product type are attached; "D" if the labels
+are detached.
+"""
+
+from pathlib import Path
+import pdr_tests
+
+MANIFEST_DIR = Path(Path(pdr_tests.__file__).parent, "node_manifests")
+
+# shorthand variables for specific .csv files
+PPI_FILE = Path(MANIFEST_DIR, "plasm.parquet")
+
+file_information = {
+	
+	# housekeeping data; well-labeled tables
+    "hk": {
+        "manifest": PPI_FILE,
+        "fn_must_contain": ['.CSV'],
+        "url_must_contain": ['CO-S-INMS-2-PKT-U-V1.0', 'DATA'],
+        "label": "D",
+    },
+    # level 1a INMS count data; well-labeled tables
+	"level_1": {
+        "manifest": PPI_FILE,
+        "fn_must_contain": ['.CSV'],
+        "url_must_contain": ['CO-S-INMS-3-L1A-U-V1.0', 'DATA'],
+        "label": "D",
+    },
+}
