b5b2223ff888978bca704e5219802f86b71cf67a
curtiss9 <98858647+curtiss9@users.noreply.github.com>
2023-01-20 13:54:53 -0500

    Add cassini_mimi selection rules and test indices

---
+++ pdr_tests/definitions/cassini_mimi/selection_rules.py
@@ -0,0 +1,95 @@
+"""
+This is a dictionary of information about each product type.
+the keys of the dictionary are the names of each product type. This will
+translate into local file and/or directory names for various outputs of
+pdr-tests: index files, data downloads, etc.
+
+These selection rules are ideally for the _largest individual file_ of each
+product (which in some cases will be the only file; lucky you, if so).
+
+manifest: the .parquet file that contains urls, file sizes, etc., scraped directly
+from the hosting institution for this product type. for some data sets, all
+urls will be found in the same .parquet file; for others, they may be split
+between nodes or scraping sessions.
+
+fn_must_contain: a list of strings that must be in the file name (not counting
+directories, domains, etc.) to differentiate it from the other types in the
+manifest file
+
+url_must_contain: an optional additional list of strings that must be in the
+url (counting the entire url) to differentiate this from other types in the
+manifest file. useful for specifying directories.
+
+label: "A" if the labels for this product type are attached; "D" if the labels
+are detached.
+"""
+
+from pathlib import Path
+import pdr_tests
+
+MANIFEST_DIR = Path(Path(pdr_tests.__file__).parent, "node_manifests")
+
+# shorthand variables for specific .csv files
+PPI_FILE = Path(MANIFEST_DIR, "plasm_full.parquet")
+
+file_information = {
+	
+	# uncalibrated data from CHEMS sensor
+    "edr_chems": {
+        "manifest": PPI_FILE,
+        "fn_must_contain": ['.CSV'],
+        "url_must_contain": ['CO-E_J_S_SW-MIMI-2-CHEMS-UNCALIB', 'DATA'],
+        "label": "D",
+	},
+	# uncalibrated data from INCA sensor
+    "edr_inca": {
+        "manifest": PPI_FILE,
+        "fn_must_contain": ['.CSV'],
+        "url_must_contain": ['CO-E_J_S_SW-MIMI-2-INCA-UNCALIB', 'DATA'],
+        "label": "D",
+	},
+	# uncalibrated data from LEMMS sensor
+    "edr_lemms": {
+        "manifest": PPI_FILE,
+        "fn_must_contain": ['.CSV'],
+        "url_must_contain": ['CO-E_J_S_SW-MIMI-2-LEMMS-UNCALIB', 'DATA'],
+        "label": "D",
+	},
+	
+	# calibrated CHEMS data
+    "rdr_chems_avg": {
+        "manifest": PPI_FILE,
+        "fn_must_contain": ['.CSV', 'AVG'],
+        "url_must_contain": ['CO-S-MIMI-4-CHEMS-CALIB', 'DATA/C'],
+        "label": "D",
+	},
+    "rdr_chems_fullres": {
+        "manifest": PPI_FILE,
+        "fn_must_contain": ['.csv', 'FULL'],
+        "url_must_contain": ['CO-S-MIMI-4-CHEMS-CALIB', 'DATA/C'],
+        "label": "D",
+	},
+	
+	# calibrated INCA data
+    "rdr_inca": {
+        "manifest": PPI_FILE,
+        "fn_must_contain": ['.CSV'],
+        "url_must_contain": ['CO-S-MIMI-4-INCA-CALIB', 'DATA/I'],
+        "label": "D",
+	},
+
+	# calibrated LEMMS data
+    "rdr_lemms_avg": {
+        "manifest": PPI_FILE,
+        "fn_must_contain": ['.CSV', 'AVG'],
+        "url_must_contain": ['CO-S-MIMI-4-LEMMS-CALIB', 'DATA/L'],
+        "label": "D",
+	},
+	"rdr_lemms_fullres": {
+        "manifest": PPI_FILE,
+        "fn_must_contain": ['.csv', 'FULL'],
+        "url_must_contain": ['CO-S-MIMI-4-LEMMS-CALIB', 'DATA/L'],
+        "label": "D",
+	},
+	
+}
