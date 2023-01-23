d0d0d1d9329cf2802da481fba7401891fbc96106
curtiss9 <98858647+curtiss9@users.noreply.github.com>
2023-01-23 13:45:14 -0500

    Add cassini_uvis selection rules and test indices

---
+++ pdr_tests/definitions/cassini_uvis/selection_rules.py
@@ -0,0 +1,60 @@
+"""
+This is a dictionary of information about each product type.
+the keys of the dictionary are the names of each product type. This will
+translate into local file and/or directory names for various outputs of
+pdr-tests: index files, data downloads, etc.
+
+These selection rules are ideally for the _largest individual file_ of each
+product (which in some cases will be the only file; lucky you, if so).
+
+manifest: the .parquet file that contains urls, file sizes, etc., scraped directly
+from the hosting institution for this product type. for some data sets, all
+urls will be found in the same .parquet file; for others, they may be split
+between nodes or scraping sessions.
+
+fn_must_contain: a list of strings that must be in the file name (not counting
+directories, domains, etc.) to differentiate it from the other types in the
+manifest file
+
+url_must_contain: an optional additional list of strings that must be in the
+url (counting the entire url) to differentiate this from other types in the
+manifest file. useful for specifying directories.
+
+label: "A" if the labels for this product type are attached; "D" if the labels
+are detached.
+"""
+
+from pathlib import Path
+import pdr_tests
+
+MANIFEST_DIR = Path(Path(pdr_tests.__file__).parent, "node_manifests")
+
+# shorthand variables for specific .csv files
+ATM_FILE = Path(MANIFEST_DIR, "atm.parquet")
+
+file_information = {
+	
+    # Hydrogen-Deuterium Absorption Cell; TIME_SERIES
+    "hdac": {
+        "manifest": ATM_FILE,
+        "fn_must_contain": ['HDAC', '.DAT'],
+        "url_must_contain": ['couvis', 'DATA'],
+        "label": "D",
+    },
+    # Far Ultraviolet; QUBE
+    "fuv": {
+        "manifest": ATM_FILE,
+        "fn_must_contain": ['FUV', '.DAT'],
+        "url_must_contain": ['couvis', 'DATA'],
+        "label": "D",
+    },
+    # Extreme Ultraviolet; QUBE
+    "euv": {
+        "manifest": ATM_FILE,
+        "fn_must_contain": ['EUV', '.DAT'],
+        "url_must_contain": ['couvis', 'DATA'],
+        "label": "D",
+    },
+    
+	
+}
