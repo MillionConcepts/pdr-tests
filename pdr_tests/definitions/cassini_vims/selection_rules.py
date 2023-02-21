1156478a283f18d7c539dd0abd6b416cf30d3c1f
curtiss9 <98858647+curtiss9@users.noreply.github.com>
2023-02-21 13:51:10 -0500

    Add cassini_vims selection rules and test index

---
+++ pdr_tests/definitions/cassini_vims/selection_rules.py
@@ -0,0 +1,45 @@
+"""
+This is a dictionary of information about each product type.
+the keys of the dictionary are the names of each product type. This will
+translate into local file and/or directory names for various outputs of
+pdr-tests: index files, data downloads, etc.
+
+These selection rules are ideally for the _largest individual file_ of each
+product (which in some cases will be the only file; lucky you, if so).
+
+manifest: the .parquet file that contains urls, file sizes, etc., scraped directly
+from the hosting institution for this product type. for some data sets, all
+urls will be found in the same .parquet file; for others, they may be split
+between nodes or scraping sessions.
+
+fn_must_contain: a list of strings that must be in the file name (not counting
+directories, domains, etc.) to differentiate it from the other types in the
+manifest file
+
+url_must_contain: an optional additional list of strings that must be in the
+url (counting the entire url) to differentiate this from other types in the
+manifest file. useful for specifying directories.
+
+label: "A" if the labels for this product type are attached; "D" if the labels
+are detached.
+"""
+
+from pathlib import Path
+import pdr_tests
+
+MANIFEST_DIR = Path(Path(pdr_tests.__file__).parent, "node_manifests")
+
+RMS_PDS4 = Path(MANIFEST_DIR, "ringpds4.parquet")
+
+file_information = {
+	
+	# spectral cube EDRs - pds3 fails to open, pds4 opens fine
+    "cube": {
+        "manifest": RMS_PDS4,
+        "fn_must_contain": ['.qub'],
+        "url_must_contain": ['cassini_vims_saturn/data_raw'],
+        "label": ('.qub', '.xml'),
+    },
+    
+	
+}
