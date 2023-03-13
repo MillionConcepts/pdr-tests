132b2078a9b076208802d2081578a6d293579ed3
curtiss9 <98858647+curtiss9@users.noreply.github.com>
2023-03-13 16:43:53 -0400

    Add mgn_occult selection rules and test indices

---
+++ pdr_tests/definitions/mgn_occult/selection_rules.py
@@ -0,0 +1,61 @@
+"""
+This is a dictionary of information about each product type.
+the keys of the dictionary are the names of each product type. This will
+translate into local file and/or directory names for various outputs of
+pdr-tests: index files, data downloads, etc.
+
+These selection rules are ideally for the _largest individual file_ of each
+product (which in some cases will be the only file; lucky you, if so).
+
+manifest: the .parquet file that contains urls, file sizes, etc., scraped directly
+from the hosting institution for this product type. for some data sets, all
+urls will be found in the same .parquet file; for others, they may be split
+between nodes or scraping sessions.
+
+fn_must_contain: a list of strings that must be in the file name (not counting
+directories, domains, etc.) to differentiate it from the other types in the
+manifest file
+
+url_must_contain: an optional additional list of strings that must be in the
+url (counting the entire url) to differentiate this from other types in the
+manifest file. useful for specifying directories.
+
+label: "A" if the labels for this product type are attached; "D" if the labels
+are detached.
+"""
+
+from pathlib import Path
+import pdr_tests
+
+MANIFEST_DIR = Path(Path(pdr_tests.__file__).parent, "node_manifests")
+
+ATM_MANIFEST = Path(MANIFEST_DIR, "atm.parquet")
+
+file_information = {
+	
+	# "Magellan Radio Occultation Raw Data Archive"
+    # Original Data Record (ODR) --> primary data type
+    "odr": {
+        "manifest": ATM_MANIFEST,
+        "fn_must_contain": ['.odr'],
+        "url_must_contain": ['mg_22', 'odr'],
+        "label": "D",
+    },
+    # Tracking Data Files (TDFs) --> backup to ODRs, ancillary
+    "tdf": {
+        "manifest": ATM_MANIFEST,
+        "fn_must_contain": ['.tdf'],
+        "url_must_contain": ['mg_22', 'tdf'],
+        "label": "D",
+    },
+    # "Magellan Venus Radio Occultation Atmospheric Profiles Data Set Archive"
+    # derived products; 2 profiles (ABS and RTPD)
+    "ddr": {
+        "manifest": ATM_MANIFEST,
+        "fn_must_contain": ['.dat'],
+        "url_must_contain": ['mg_2401/data'],
+        "label": "D",
+    },
+    
+	
+}
