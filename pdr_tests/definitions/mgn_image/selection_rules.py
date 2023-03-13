4270ae87eb8db424993508e19e788e86596443e6
curtiss9 <98858647+curtiss9@users.noreply.github.com>
2023-03-13 16:26:33 -0400

    Add mgn_image (MIDR and BIDR) selection rules and test indices

---
+++ pdr_tests/definitions/mgn_image/selection_rules.py
@@ -0,0 +1,86 @@
+"""
+This is a dictionary of information about each product type.
+the keys of the dictionary are the names of each product type. This will
+translate into local file and/or directory names for various outputs of
+pdr-tests: index files, data downloads, etc.
+
+These selection rules are ideally for the _largest individual file_ of each
+product (which in some cases will be the only file; lucky you, if so).
+
+manifest: the .parquet file that contains urls, file sizes, etc., scraped directly
+from the hosting institution for this product type. for some data sets, all
+urls will be found in the same .parquet file; for others, they may be split
+between nodes or scraping sessions.
+
+fn_must_contain: a list of strings that must be in the file name (not counting
+directories, domains, etc.) to differentiate it from the other types in the
+manifest file
+
+url_must_contain: an optional additional list of strings that must be in the
+url (counting the entire url) to differentiate this from other types in the
+manifest file. useful for specifying directories.
+
+label: "A" if the labels for this product type are attached; "D" if the labels
+are detached.
+"""
+
+from pathlib import Path
+import pdr_tests
+
+MANIFEST_DIR = Path(Path(pdr_tests.__file__).parent, "node_manifests")
+
+GEO_MANIFEST = Path(MANIFEST_DIR, "geomgn.parquet")
+
+file_information = {
+	
+    # Compressed-Resolution Basic Image Data Record
+    "c-bidr": {
+        "manifest": GEO_MANIFEST,
+        "fn_must_contain": ['IM', '.DAT'], # image data products only
+        "url_must_contain": ['mgn-v-rdrs-5-c-bidr-v1'],
+        "label": "D",
+    },
+    # ancillary tables; known unsupported 
+    # labels do not define number of columns for tables, headers are being sent to read_table()
+    # "c-bidr_tables": {
+    #     "manifest": GEO_MANIFEST,
+    #     "fn_must_contain": ['.DAT'],
+    #     "fn_regex": [r'^[A-HJ-Z]'],
+    #     "url_must_contain": ['mgn-v-rdrs-5-c-bidr-v1'],
+    #     "label": "D",
+    # },
+	
+    # Mosaic Image Data Record
+    # full resolution
+    "f-midr": {
+        "manifest": GEO_MANIFEST,
+        "fn_must_contain": ['.img'],
+        "url_must_contain": ['mgn-v-rdrs-5-midr-full-res-v1'],
+        "url_regex": [r'mg_..../f'],
+        "label": "D",
+    },
+    # compressed
+    "c-midr": {
+        "manifest": GEO_MANIFEST,
+        "fn_must_contain": ['.img'],
+        "url_must_contain": ['mgn-v-rdrs-5-midr-full-res-v1'],
+        "url_regex": [r'mg_..../c'],
+        "label": "D",
+    },
+    # each MIDR volume/subdirectory has frame, geometry, and histogram tables
+    "midr_tables": {
+        "manifest": GEO_MANIFEST,
+        "fn_must_contain": ['.tab'],
+        "url_must_contain": ['mgn-v-rdrs-5-midr-full-res-v1'],
+        "label": "D",
+    },
+    
+#     # Full-Resolution Basic Image Data Record --> no pointers, not archive compliant
+#     "f-bidr": {
+#         "manifest": GEO_MANIFEST,
+#         "fn_must_contain": [''],
+#         "url_must_contain": ['mgn-v-rdrs-5-bidr-full-res-v1'],
+#         "label": "D",
+#     },
+	
+}
