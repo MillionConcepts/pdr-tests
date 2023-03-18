7a3aa1a23311cf51d8f70836c0e61d84222f88ee
michael <mstclair@millionconcepts.com>
2023-03-18 18:51:16 -0400

    make ix_help slightly less likely to pop up

--- pdr_tests/ix.py
+++ pdr_tests/ix.py
@@ -18,10 +18,11 @@ def handle_call():
         assert command in ix_interface.COMMANDS, (
             f"{command} not " f"in {ix_interface.COMMANDS}"
         )
-        run(getattr(ix_interface, command), args=sys.argv[1:])
     except (IndexError, AttributeError, AssertionError) as ex:
         print(ex)
-        ix_interface.ix_help()
+        return ix_interface.ix_help()
+    run(getattr(ix_interface, command), args=sys.argv[1:])
+
 
 
 if __name__ == "__main__":
