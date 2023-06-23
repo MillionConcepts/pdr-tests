43a868e27de4263df0073da20a5df563f7ea9759
curtiss9 <98858647+curtiss9@users.noreply.github.com>
2023-06-23 12:56:37 -0400

    add Stardust and NExT selection rules and test indices

---
+++ pdr_tests/definitions/stardust-next/selection_rules.py
@@ -0,0 +1,72 @@
+"""
+This is a dictionary of information about each product type.
+the keys of the dictionary are the names of each product type. This will
+translate into local file and/or directory names for various outputs of
+pdr-tests: index files, data downloads, etc.
+
+These selection rules are ideally for the _largest individual file_ of each
+product (which in some cases will be the only file; lucky you, if so).
+
+manifest: the .parquet file that contains urls, file sizes, etc., scraped directly
+from the hosting institution for this product type. for some data sets, all
+urls will be found in the same .parquet file; for others, they may be split
+between nodes or scraping sessions.
+
+fn_must_contain: a list of strings that must be in the file name (not counting
+directories, domains, etc.) to differentiate it from the other types in the
+manifest file
+
+url_must_contain: an optional additional list of strings that must be in the
+url (counting the entire url) to differentiate this from other types in the
+manifest file. useful for specifying directories.
+
+label: "A" if the labels for this product type are attached; "D" if the labels
+are detached.
+"""
+
+from pathlib import Path
+import pdr_tests
+
+MANIFEST_DIR = Path(Path(pdr_tests.__file__).parent, "node_manifests")
+
+# shorthand variables for specific .csv files
+SB_FILE = Path(MANIFEST_DIR, "tiny.parquet")
+
+file_information = {
+    # NAVCAM - EDR
+    "nav_edr": {
+        "manifest": SB_FILE,
+        "fn_must_contain": ['.fit'],
+        "url_must_contain": ['sdu-c_cal-navcam-2-next-tempel1-v1.0/data'],
+        "label": "D",
+    },
+    # NAVCAM - RDR
+    "nav_rdr": {
+        "manifest": SB_FILE,
+        "fn_must_contain": ['.fit'],
+        "url_must_contain": ['sdu-c_cal-navcam-3-next-tempel1-v1.0/data'],
+        "label": "D",
+    },
+    # Derived Shape Model
+    "shape": {
+        "manifest": SB_FILE,
+        "fn_regex": ['(tab$)|(wrl$)'],
+        "url_must_contain": ['dif-c-hriv_its_mri-5-tempel1-shape-v2.0/data'],
+        "label": "D",
+    },
+    # DFMI
+    "dfmi": {
+        "manifest": SB_FILE,
+        "fn_must_contain": ['.tab'],
+        "url_must_contain": ['sdu-c_d-dfmi-2_3-next-tempel1-v1.0/data'],
+        "label": "D",
+    },
+    # CIDA
+    "cida": {
+        "manifest": SB_FILE,
+        "fn_must_contain": ['.tab'],
+        "url_must_contain": ['sdu-c_d-cida-2_3-next-tempel1-v1.0/data'],
+        "label": "A",
+    },
+    
+}
---
+++ pdr_tests/definitions/stardust/selection_rules.py
@@ -0,0 +1,112 @@
+"""
+This is a dictionary of information about each product type.
+the keys of the dictionary are the names of each product type. This will
+translate into local file and/or directory names for various outputs of
+pdr-tests: index files, data downloads, etc.
+
+These selection rules are ideally for the _largest individual file_ of each
+product (which in some cases will be the only file; lucky you, if so).
+
+manifest: the .parquet file that contains urls, file sizes, etc., scraped directly
+from the hosting institution for this product type. for some data sets, all
+urls will be found in the same .parquet file; for others, they may be split
+between nodes or scraping sessions.
+
+fn_must_contain: a list of strings that must be in the file name (not counting
+directories, domains, etc.) to differentiate it from the other types in the
+manifest file
+
+url_must_contain: an optional additional list of strings that must be in the
+url (counting the entire url) to differentiate this from other types in the
+manifest file. useful for specifying directories.
+
+label: "A" if the labels for this product type are attached; "D" if the labels
+are detached.
+"""
+
+from pathlib import Path
+import pdr_tests
+
+MANIFEST_DIR = Path(Path(pdr_tests.__file__).parent, "node_manifests")
+
+# shorthand variables for specific .csv files
+SB_FILE = Path(MANIFEST_DIR, "tiny.parquet")
+
+file_information = {
+    # Sample Return Capsule (SRC) - temperature
+    "src_temp": {
+        "manifest": SB_FILE,
+        "fn_must_contain": ['.tab'],
+        "url_must_contain": ['sdu-c-src-2-temps-v1.0/data'],
+        "label": "D",
+    },
+    # SRC - geometry
+    "src_geom": {
+        "manifest": SB_FILE,
+        "fn_must_contain": ['.tab'],
+        "url_must_contain": ['sdu-c-src-6-geometry-v1.0/data'],
+        "label": "D",
+    },
+    # Cometary and Interstellar Dust Analyzer (CIDA) - EDF
+    "cida": {
+        "manifest": SB_FILE,
+        "fn_must_contain": ['.tab'],
+        "url_must_contain": ['sdu-c_d-cida-1-edf_hk-v1.0/data/edfascii'],
+        "label": "A",
+    },
+    # CIDA - housekeeping
+    "cida_hk": {
+        "manifest": SB_FILE,
+        "fn_must_contain": ['.tab'],
+        "url_must_contain": ['sdu-c_d-cida-1-edf_hk-v1.0/data/hkvalues'],
+        "label": "A",
+    },
+    # NAVCAM - pre-flight calibration
+    # (skipping testing for products in the "unusable" directory)
+    "nav_cal": {
+        "manifest": SB_FILE,
+        "fn_must_contain": ['.fit'],
+        "url_must_contain": ['stardust-cal-nc-2-preflight-v2.0/data/minus'],
+        "label": "D",
+    },
+    # NAVCAM - raw images
+    "nav_edr": {
+        "manifest": SB_FILE,
+        "fn_must_contain": ['.fit'],
+        "url_must_contain": ['sdu-', '-navcam-2-edr-', '/data'],
+        "label": "D",
+    },
+    # NAVCAM - calibrated images
+    "nav_rdr": {
+        "manifest": SB_FILE,
+        "fn_must_contain": ['.fit'],
+        "url_must_contain": ['sdu-', '-navcam-3-rdr-', '/data'],
+        "label": "D",
+    },
+    # NAVCAM - derived shape models
+    "nav_ddr": {
+        "manifest": SB_FILE,
+        "fn_must_contain": ['.tab'],
+        "url_must_contain": ['sdu-c-navcam-5-wild2-shape-model-v2.1/data'],
+        "label": "D",
+    },
+    # Dust Flux Monitor (DFMI) - EDR
+    "dfmi": {
+        "manifest": SB_FILE,
+        "fn_must_contain": ['.tab'],
+        "url_must_contain": ['sdu-c-dfmi-2-edr-wild2-v1.0/data'],
+        "label": "D",
+    },
+    # Dynamic Science Experiment (DSE)
+    "dse": {
+        "manifest": SB_FILE,
+        "fn_must_contain": ['.tab'],
+        "url_must_contain": ['sdu-c-dynsci-2-wild2-v1.0/data'],
+        "label": "D",
+    },
+
+    # SAFED datasets that are being skipped for testing:
+    # STARDUST-C/E/L-DFMI-2-EDR-V1.0 - early cruise phase DFMI data
+    # STARDUST-C/E/L-NC-2-EDR-V1.0 - early cruise phase NAVCAM images
+    # STARDUST-CAL-NC-2-PREFLIGHT-V1.0
+}
