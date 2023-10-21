2a107206eb43d132d33b5f8237d24540f90eb9ea
curtiss9 <98858647+curtiss9@users.noreply.github.com>
2023-10-20 21:14:02 -0400

    add Voyager PLS selection rules and test indices

---
+++ pdr_tests/definitions/vg_pls/selection_rules.py
@@ -0,0 +1,152 @@
+"""
+This is a dictionary of information about each product type.
+the keys of the dictionary are the names of each product type. This will
+translate into local file and/or directory names for various outputs of
+pdr-tests: index files, data downloads, etc.
+
+These selection rules are ideally for the _largest individual file_ of each
+product (which in some cases will be the only file; lucky you, if so).
+
+manifest: the .parquet file that contains urls, file sizes, etc., scraped
+directly from the hosting institution for this product type. for some data
+sets, all urls will be found in the same .parquet file; for others, they may
+be split between nodes or scraping sessions.
+
+fn_must_contain: a list of strings that must be in the file name (not counting
+directories, domains, etc.) to differentiate it from the other types in the
+manifest file
+
+url_must_contain: an optional additional list of strings that must be in the
+url (counting the entire url) to differentiate this from other types in the
+manifest file. useful for specifying directories.
+
+label: "A" if the labels for this product type are attached; "D" if the labels
+are detached.
+"""
+
+from pathlib import Path
+import pdr_tests
+
+MANIFEST_DIR = Path(Path(pdr_tests.__file__).parent, "node_manifests")
+
+# shorthand variables for specific .csv files
+PPI_FILE = Path(MANIFEST_DIR, "plasm_full.parquet")
+
+file_information = {
+
+    # PDS4: VG1 Jupiter, Saturn, and Solar System; VG2 Jupiter
+    
+    # VG2 Saturn
+    "sat_ascii": {
+        "manifest": PPI_FILE,
+        "fn_must_contain": ['.TAB'],
+        "url_must_contain": ['VG2-S-PLS-5-SUMM', '/DATA'],
+        "label": "D",
+    },
+
+    # VG2 Uranus
+    "ur_elefit": {
+        "manifest": PPI_FILE,
+        "fn_must_contain": ['.DAT'],
+        "url_must_contain": ['VG2-U-PLS-5-RDR-ELEFIT', '/DATA'],
+        "label": "D",
+    },
+    "ur_ionfit": {
+        "manifest": PPI_FILE,
+        "fn_must_contain": ['.DAT'],
+        "url_must_contain": ['VG2-U-PLS-5-RDR-IONFIT', '/DATA'],
+        "label": "D",
+    },
+    "ur_elebr": {
+        "manifest": PPI_FILE,
+        "fn_must_contain": ['.DAT'],
+        "url_must_contain": ['VG2-U-PLS-5-SUMM-ELEBR', '/DATA'],
+        "label": "D",
+    },
+    "ur_ionbr": {
+        "manifest": PPI_FILE,
+        "fn_must_contain": ['.DAT'],
+        "url_must_contain": ['VG2-U-PLS-5-SUMM-IONBR', '/DATA'],
+        "label": "D",
+    },
+    # ascii versions of the 4 product types above
+    "ur_ascii": {
+        "manifest": PPI_FILE,
+        "fn_must_contain": ['.TAB'],
+        "url_must_contain": ['VG2-U-PLS-5', '/DATA'],
+        "label": "D",
+    },
+    
+    # VG2 Neptune
+    "nep_pro_sphere": {
+        "manifest": PPI_FILE,
+        "fn_must_contain": ['.DAT'],
+        "url_must_contain": ['VG2-N-PLS-5-RDR-2PROMAGSPH', '/DATA'],
+        "label": "D",
+    },
+    # Known unsupported; missing pointers in the labels.
+    # Adding pointers manually leads to an error: "UserWarning: Unable to load
+    # TIME_SERIES: 'utf-8' codec can't decode byte 0xc6 in position 1: invalid
+    # continuation byte"
+##    "nep_ele_sphere": {
+##        "manifest": PPI_FILE,
+##        "fn_must_contain": ['.DAT'],
+##        "url_must_contain": ['VG2-N-PLS-5-RDR-ELEMAGSPHERE', '/DATA'],
+##        "label": "D",
+##    },
+    "nep_wind": {
+        "manifest": PPI_FILE,
+        "fn_must_contain": ['.DAT'],
+        "url_must_contain": ['VG2-N-PLS-5-RDR-IONINBNDWIND', '/DATA'],
+        "label": "D",
+    },
+    "nep_l_mode": {
+        "manifest": PPI_FILE,
+        "fn_must_contain": ['.DAT'],
+        "url_must_contain": ['VG2-N-PLS-5-RDR-IONLMODE', '/DATA'],
+        "label": "D",
+    },
+    "nep_ion_sphere": {
+        "manifest": PPI_FILE,
+        "fn_must_contain": ['.DAT'],
+        "url_must_contain": ['VG2-N-PLS-5-RDR-IONMAGSPHERE', '/DATA'],
+        "label": "D",
+    },
+    "nep_m_mode": {
+        "manifest": PPI_FILE,
+        "fn_must_contain": ['.DAT'],
+        "url_must_contain": ['VG2-N-PLS-5-RDR-IONMMODE', '/DATA'],
+        "label": "D",
+    },
+    # ascii versions of the 6 product types above
+    "nep_ascii": {
+        "manifest": PPI_FILE,
+        "fn_must_contain": ['.TAB'],
+        "url_must_contain": ['VG2-N-PLS-5-RDR', '/DATA'],
+        "label": "D",
+    },
+    
+    # VG2 Solar System
+    # fine resolution plasma data
+    "sys_fine_res": {
+        "manifest": PPI_FILE,
+        "fn_must_contain": ['.TAB'],
+        "url_must_contain": ['VG2-SS-PLS-3-RDR-FINE-RES', '/DATA'],
+        "label": "D",
+    },
+    # plasam daily averages
+    "sys_1day_avg": {
+        "manifest": PPI_FILE,
+        "fn_must_contain": ['.TAB'],
+        "url_must_contain": ['VG2-SS-PLS-4-SUMM-1DAY-AVG', '/DATA'],
+        "label": "D",
+    },
+    # plasma hourly averages
+    "sys_1hr_avg": {
+        "manifest": PPI_FILE,
+        "fn_must_contain": ['.TAB'],
+        "url_must_contain": ['VG2-SS-PLS-4-SUMM-1HR-AVG', '/DATA'],
+        "label": "D",
+    },
+}
+
