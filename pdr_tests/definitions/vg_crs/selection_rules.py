3374ec3f71c156e79942da566968db53414457bc
curtiss9 <98858647+curtiss9@users.noreply.github.com>
2023-11-01 16:00:29 -0400

    add Voyager CRS selection rules and test indices

---
+++ pdr_tests/definitions/vg_crs/selection_rules.py
@@ -0,0 +1,88 @@
+"""
+This is a dictionary of information about each product type.
+the keys of the dictionary are the names of each product type. This will
+translate into local file and/or directory names for various outputs of
+pdr-tests: index files, data downloads, etc.
+
+These selection rules are ideally for the _largest individual file_ of each
+product (which in some cases will be the only file; lucky you, if so).
+
+manifest: the .parquet file that contains urls, file sizes, etc., scraped
+directly from the hosting institution for this product type. for some data
+sets, all urls will be found in the same .parquet file; for others, they may
+be split between nodes or scraping sessions.
+
+fn_must_contain: a list of strings that must be in the file name (not counting
+directories, domains, etc.) to differentiate it from the other types in the
+manifest file
+
+url_must_contain: an optional additional list of strings that must be in the
+url (counting the entire url) to differentiate this from other types in the
+manifest file. useful for specifying directories.
+
+label: "A" if the labels for this product type are attached; "D" if the labels
+are detached.
+"""
+
+from pathlib import Path
+import pdr_tests
+
+MANIFEST_DIR = Path(Path(pdr_tests.__file__).parent, "node_manifests")
+
+# shorthand variables for specific .csv files
+PPI_FILE = Path(MANIFEST_DIR, "plasm_full.parquet")
+
+file_information = {
+    
+    # PDS4: Jupiter (VG1/VG2) and Saturn (VG1)
+    
+    # RDR (Neptune, VG2)
+    "rdr": {
+        "manifest": PPI_FILE,
+        "fn_regex": ['(TAB)|(DAT)$'],
+        "url_must_contain": ['VG2-N-CRS-3-RDR-D1-6SEC-V1.0/DATA'],
+        "label": "D",
+    },
+    # SUMM D1/D2 (Saturn, VG2)
+    "summ_saturn": {
+        "manifest": PPI_FILE,
+        "fn_must_contain": ['.TAB'],
+        "url_must_contain": ['VG2-S-CRS-4-SUMM-D1_D2-192SEC-V1.0/DATA'],
+        "label": "D",
+    },
+    
+    # The binary products are grouped by detector and the ascii products are
+    # grouped by target. This is to avoid repeated filenames ending up in the
+    # same index/folder during ix testing.
+    
+    # SUMM D1 (Binary products, Uranus and Neptune, VG2)
+    "summ_d1": {
+        "manifest": PPI_FILE,
+        "fn_must_contain": ['.DAT'],
+        "url_must_contain": ['VG2-', '-CRS-4-SUMM-D1-96SEC-V1.0/DATA'],
+        "label": "D",
+    },
+    # SUMM D2 (Binary products, Uranus and Neptune, VG2)
+    "summ_d2": {
+        "manifest": PPI_FILE,
+        "fn_must_contain": ['.DAT'],
+        "url_must_contain": ['VG2-', '-CRS-4-SUMM-D2-96SEC-V1.0/DATA'],
+        "label": "D",
+    },
+    # SUMM Uranus (Ascii products, D1 and D2, VG2)
+    "summ_uranus": {
+        "manifest": PPI_FILE,
+        "fn_must_contain": ['.TAB'],
+        "url_must_contain": ['VG2-U-CRS-4-SUMM-D', '-96SEC-V1.0/DATA'],
+        "label": "D",
+    },
+    # SUMM Neptune (Ascii products, D1 and D2, VG2)
+    "summ_neptune": {
+        "manifest": PPI_FILE,
+        "fn_must_contain": ['.TAB'],
+        "url_must_contain": ['VG2-N-CRS-4-SUMM-D', '-96SEC-V1.0/DATA'],
+        "label": "D",
+    },
+    
+}
+
