3516d82e691ee7f345b0d0dbe0edd401d416d98e
curtiss9 <98858647+curtiss9@users.noreply.github.com>
2023-11-14 14:51:17 -0500

    add mgs_rss_edr selection rules and test indices

---
+++ pdr_tests/definitions/mgs_rss_eds/selection_rules.py
@@ -0,0 +1,52 @@
+"""
+This is a dictionary of information about each product type.
+the keys of the dictionary are the names of each product type. This will
+translate into local file and/or directory names for various outputs of
+pdr-tests: index files, data downloads, etc.
+
+These selection rules are ideally for the _largest individual file_ of each
+product (which in some cases will be the only file; lucky you, if so).
+
+manifest: the .parquet file that contains urls, file sizes, etc., scraped directly
+from the hosting institution for this product type. for some data sets, all
+urls will be found in the same .parquet file; for others, they may be split
+between nodes or scraping sessions.
+
+fn_must_contain: a list of strings that must be in the file name (not counting
+directories, domains, etc.) to differentiate it from the other types in the
+manifest file
+
+url_must_contain: an optional additional list of strings that must be in the
+url (counting the entire url) to differentiate this from other types in the
+manifest file. useful for specifying directories.
+
+label: "A" if the labels for this product type are attached; "D" if the labels
+are detached.
+"""
+
+from pathlib import Path
+import pdr_tests
+
+MANIFEST_DIR = Path(Path(pdr_tests.__file__).parent, "node_manifests")
+
+# shorthand variables for specific .csv files
+MANIFEST_FILE = Path(MANIFEST_DIR, "plasm_full.parquet")
+
+file_information = {
+        
+    # radio occultation ionospheric electron density profiles
+    "eds": {
+        "manifest": MANIFEST_FILE,
+        "fn_must_contain": ['.EDS'],
+        "url_must_contain": ['MGS-M-RSS-5-EDS-V1.0/DATA/RS_EDS'],
+        "label": "D",
+    },
+    # summary table of the eds profiles
+    "ocs": {
+        "manifest": MANIFEST_FILE,
+        "fn_must_contain": ['.OCS'],
+        "url_must_contain": ['MGS-M-RSS-5-EDS-V1.0/DATA/RS_OCS'],
+        "label": "D",
+    },
+    
+}
