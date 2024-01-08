4f9d846e031733845dbf2d8e63415bdcca8ae24b
curtiss9 <98858647+curtiss9@users.noreply.github.com>
2024-01-08 17:54:23 -0500

    add selection rules and test indices for misc ATM node datasets

---
+++ pdr_tests/definitions/ground-based/selection_rules.py
@@ -0,0 +1,39 @@
+"""
+This is a dictionary of information about each product type. 
+the keys of the dictionary are the names of each product type. This will
+translate into local file and/or directory names for various outputs of
+pdr-tests: index files, data downloads, etc.
+
+These selection rules are ideally for the _largest individual file_ of each
+product (which in some cases will be the only file; lucky you, if so).
+
+manifest: the .parquet file that contains urls, file sizes, etc., scraped directly
+from the hosting institution for this product type. for some data sets, all
+urls will be found in the same .parquet file; for others, they may be split
+between nodes or scraping sessions.
+
+fn_must_contain: a list of strings that must be in the file name (not counting
+directories, domains, etc.) to differentiate it from the other types in the
+manifest file
+
+url_must_contain: an optional additional list of strings that must be in the 
+url (counting the entire url) to differentiate this from other types in the
+manifest file. useful for specifying directories.
+
+label: "A" if the labels for this product type are attached; "D" if the labels
+are detached.
+"""
+# variables naming specific parquet files in node_manifests
+MANIFEST_FILE = "atm"
+
+file_information = {
+    # Ground-based spectrophotometry of the Jovian planets and Titan.
+    # Methane absorption and albedos
+    "gbat": {
+        "manifest": MANIFEST_FILE,
+        "fn_must_contain": ['.tab'],
+        "url_must_contain": ['gbat_0001/data'],
+        "label": "D",
+    },
+    
+}
