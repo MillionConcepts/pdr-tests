8b0445c8e9622d00ba94da23108c2fbdc594b519
curtiss9 <98858647+curtiss9@users.noreply.github.com>
2024-05-14 17:59:03 -0400

    Rework the cassini_cirs selection rules and test cases

--- pdr_tests/definitions/cassini_cirs/selection_rules.py
+++ pdr_tests/definitions/cassini_cirs/selection_rules.py
@@ -28,79 +28,160 @@ are detached.
 ATM_FILE = "atm"
 RMS_FILE = "ringvolumes"
 
+"""
+Some ptypes have fixed-length and variable-length tables associated to each
+product. There are no pointers to the variable-length tables/files.
+Product types with .VAR files: frv, ifgm, ispm, and oispm
+"""
+
 file_information = {
-	
-    # navigational data (GEO, POI, RIN, TAR); fixed-length
-    "nav": {
+    
+    # APODSPEC
+    # ISPM - calibrated, re-sampled (interpolated) spectral data
+    "ispm": {
         "manifest": ATM_FILE,
-        "fn_must_contain": ['.DAT'],
+        "fn_must_contain": ['ISPM', '.DAT'],
+        "url_must_contain": ['cocirs', 'DATA/TSDR/APODSPEC'],
+        "label": "D",
+    },
+    # HSK_DATA
+    # HSK - raw housekeeping data
+    "hsk": {
+        "manifest": ATM_FILE,
+        "fn_must_contain": ['HSK', '.DAT'],
+        "url_must_contain": ['cocirs', 'DATA/TSDR/HSK_DATA'],
+        "label": "D",
+    },
+    # NAV_DATA
+    # GEO - spacecraft orientation data
+    "geo": {
+        "manifest": ATM_FILE,
+        "fn_must_contain": ['GEO', '.DAT'],
         "url_must_contain": ['cocirs', 'DATA/TSDR/NAV_DATA'],
         "label": "D",
     },
-    # housekeeping data (HSK, IHSK, OBS); fixed-length
-    "hsk": {
+    # POI - target body navigational data
+    "poi": {
         "manifest": ATM_FILE,
-        "fn_must_contain": ['.DAT'],
-        "fn_regex": [r'(HSK)|(IHSK)|(OBS)'],
-        "url_must_contain": ['cocirs', 'DATA/TSDR'],
-        "url_regex": [r'(HSK_DATA)|(UNCALIBR)'],
+        "fn_must_contain": ['POI', '.DAT'],
+        "url_must_contain": ['cocirs', 'DATA/TSDR/NAV_DATA'],
         "label": "D",
     },
-	# spectral QUBE (notionally supported; selected a test case for regression testing)
-    "cube": {
+    # RIN - ring observations navigational data
+    "rin": {
         "manifest": ATM_FILE,
-        "fn_must_contain": ['.tar.gz'], # contains: LBL, DAT (data file), and JPG (browse image)
-        "url_must_contain": ['cocirs', 'DATA/CUBE'],
-        "label": ('.tar.gz', '.LBL'),
+        "fn_must_contain": ['RIN', '.DAT'],
+        "url_must_contain": ['cocirs', 'DATA/TSDR/NAV_DATA'],
+        "label": "D",
     },
-    # diagnostic info for raw interferograms; fixed-length
-    # grouped with 'hsk' originally. now a separate product type because additional testing was needed
+    # TAR - target identification data
+    "tar": {
+        "manifest": ATM_FILE,
+        "fn_must_contain": ['TAR', '.DAT'],
+        "url_must_contain": ['cocirs', 'DATA/TSDR/NAV_DATA'],
+        "label": "D",
+    },
+    # SUSPECT_SPECTRA
+    # OISPM - calibrated, re-sampled spectral data of reduced quality
+    "oispm": {
+        "manifest": ATM_FILE,
+        "fn_must_contain": ['OISPM', '.DAT'],
+        "url_must_contain": ['cocirs', 'DATA/TSDR/SUSPECT_SPECTRA'],
+        "label": "D",
+    },
+    # UNCALIB
+    # DIAG - IFM diagnostic info
     "diag": {
         "manifest": ATM_FILE,
         "fn_must_contain": ['DIAG', '.DAT'],
         "url_must_contain": ['cocirs', 'DATA/TSDR/UNCALIBR'],
         "label": "D",
     },
+    # FRV - IFM fringe voltages
+    "frv": {
+        "manifest": ATM_FILE,
+        "fn_must_contain": ['FRV', '.DAT'],
+        "url_must_contain": ['cocirs', 'DATA/TSDR/UNCALIBR'],
+        "label": "D",
+    },
+    # IFGM - raw interferogram data
+    "ifgm": {
+        "manifest": ATM_FILE,
+        "fn_must_contain": ['IFGM', '.DAT'],
+        "url_must_contain": ['cocirs', 'DATA/TSDR/UNCALIBR'],
+        "label": "D",
+    },
+    # IHSK - interpolated housekeeping data 
+    "ihsk": {
+        "manifest": ATM_FILE,
+        "fn_must_contain": ['IHSK', '.DAT'],
+        "url_must_contain": ['cocirs', 'DATA/TSDR/UNCALIBR'],
+        "label": "D",
+    },
+    # OBS - observation parameters
+    "obs": {
+        "manifest": ATM_FILE,
+        "fn_must_contain": ['OBS', '.DAT'],
+        "url_must_contain": ['cocirs', 'DATA/TSDR/UNCALIBR'],
+        "label": "D",
+    },
     
-# RMS node supplemental data set
-# CIRS re-formated: simplified versions of variable-length files
-    # calibrated and resampled spectra
-    "spectra": {
+    # spectral QUBE (notionally supported; selected a test case for regression testing)
+    "cube": {
+        "manifest": ATM_FILE,
+        "fn_must_contain": ['.tar.gz'], # contains: LBL, DAT (data file), and JPG (browse image)
+        "url_must_contain": ['cocirs', 'DATA/CUBE'],
+        "label": ('.tar.gz', '.LBL'),
+    },
+    
+    # RMS node supplemental data set
+    # CIRS re-formated: simplified versions of variable-length files
+    # APODSPEC - apodized, i.e. calibrated and resampled, spectra
+    "rms_spec": {
         "manifest": RMS_FILE,
-        "fn_must_contain": ['SPEC', '.DAT'],
-        "url_must_contain": ['DATA'],
+        "fn_must_contain": ['.DAT'],
+        "url_must_contain": ['DATA/APODSPEC'],
         "url_regex": [r'(COCIRS_5xxx)|(COCIRS_6xxx)'],
         "label": "D",
     },
-	# spectra prefixes; parameters describing the calibrated spectra
-    "prefix": {
+    # GEODATA - position and viewing geometry of the planet and moons
+    "rms_geo": {
         "manifest": RMS_FILE,
-        "fn_must_contain": ['ISPM', '.TAB'],
-        "url_must_contain": ['DATA'],
+        "fn_must_contain": ['.TAB'],
+        "url_must_contain": ['DATA/GEO'],
         "url_regex": [r'(COCIRS_5xxx)|(COCIRS_6xxx)'],
         "label": "D",
     },
-		
-# 	# interferometer voltages; variable-length (not supported)
-#     "uncal_frv": {
-#         "manifest": ATM_FILE,
-#         "fn_must_contain": ['FRV', '.VAR'],
-#         "url_must_contain": ['cocirs', 'DATA/TSDR/UNCALIBR'],
-#         "label": "D",
-#     },
-#     # IFM records; variable-length (not supported)
-#     "uncal_ifgm": {
-#         "manifest": ATM_FILE,
-#         "fn_must_contain": ['IFGM', '.VAR'],
-#         "url_must_contain": ['cocirs', 'DATA/TSDR/UNCALIBR'],
-#         "label": "D",
-#     },
-# 	# calibrated spectra; variable-length (not supported)
-#     "cal_spectra": {
-#         "manifest": ATM_FILE,
-#         "fn_must_contain": ['ISPM', '.VAR'],
-#         "url_must_contain": ['cocirs', 'DATA/TSDR/APODSPEC'],
-#         "label": "D",
-#     },
-	
+    # ISPMDATA - parameters describing each spectrum
+    "rms_ispm": {
+        "manifest": RMS_FILE,
+        "fn_must_contain": ['.TAB'],
+        "url_must_contain": ['DATA/ISPMDATA'],
+        "url_regex": [r'(COCIRS_5xxx)|(COCIRS_6xxx)'],
+        "label": "D",
+    },
+    # POIDATA - pointing geometry on the planet or moon
+    "rms_poi": {
+        "manifest": RMS_FILE,
+        "fn_must_contain": ['.TAB'],
+        "url_must_contain": ['DATA/POIDATA'],
+        "url_regex": [r'(COCIRS_5xxx)|(COCIRS_6xxx)'],
+        "label": "D",
+    },
+    # RINDATA - pointing geometry on the ring system
+    "rms_rin": {
+        "manifest": RMS_FILE,
+        "fn_must_contain": ['.TAB'],
+        "url_must_contain": ['DATA/RINDATA'],
+        "url_regex": [r'(COCIRS_5xxx)|(COCIRS_6xxx)'],
+        "label": "D",
+    },
+    # TARDATA - listing of target bodies captured
+    "rms_tar": {
+        "manifest": RMS_FILE,
+        "fn_must_contain": ['.TAB'],
+        "url_must_contain": ['DATA/TARDATA'],
+        "url_regex": [r'(COCIRS_5xxx)|(COCIRS_6xxx)'],
+        "label": "D",
+    },
 }
