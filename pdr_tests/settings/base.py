422b9ab83e037c5ecea7fce139559e146c49bbcb
Zack Weinberg <zack@owlfolio.org>
2024-06-13 16:01:46 -0400

    Make data, browse, and tracker log directories configurable.
    New settings keys DATA_ROOT, BROWSE_ROOT, and TRACKER_LOG_DIR
    allow user control of where data files will be found, where
    browse products will be dumped, and where tracker logs will be
    dumped.  These default to the old hardcoded locations
    of pdr_tests/data, pdr_tests/browse, and pdr_tests/.tracker_logs
    respectively.
    
    The intention is to add command line options as well as settings
    keys, but that will wait until CLI argument parsing is redone
    since these will be common options to all ix subcommands.
    
    Also use importlib.resources to get the definition path for a
    dataset, instead of __file__ — mainly to have one fewer place
    where we use __file__, but this is also more abstractly correct
    if pdr_tests were ever to be installed as a non-editable package.
    
    ix_utils.DATA_ROOT and ix_utils.REF_ROOT are removed, as they are
    completely unused.

--- pdr_tests/settings/base.py
+++ pdr_tests/settings/base.py
@@ -3,6 +3,7 @@ from pathlib import Path
 import sys
 
 self_path = Path(__file__)
+pkg_path = self_path.parent.parent
 user_settings_path = self_path.parent / "user.py"
 if not user_settings_path.exists():
     user_settings_path.touch()
@@ -10,8 +11,12 @@ if not user_settings_path.exists():
 from pdr_tests.settings._patcher import monkeypatch_literals
 from pdr_tests.settings import user as user_settings_mod
 
+BROWSE_ROOT = pkg_path / "browse"
+DATA_ROOT = pkg_path / "data"
+MANIFEST_DIR = pkg_path / "node_manifests"
+TRACKER_LOG_DIR = pkg_path / ".tracker_logs"
+
 HEADERS = {}
-MANIFEST_DIR = self_path.parent.parent / "node_manifests"
 TEST_CORPUS_BUCKET = None
 
 monkeypatch_literals(sys.modules[__name__], user_settings_mod)
