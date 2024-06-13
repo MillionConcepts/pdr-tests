5260259ceba5566d235d5647146ce2c9ff82d976
Zack Weinberg <zack@owlfolio.org>
2024-06-13 11:33:46 -0400

    Hoist most uses of settings to the ix_interface level.
    Specifically, these functions and methods now take the manifest
    directory as an argument, instead of using settings.base.MANIFEST_DIR
    directly or indirectly:
    
    - utilz.coverage_utilz.add_coverage_column
    - utilz.coverage_utilz.check_coverage_in_chunks
    - utilz.ix_utils.find_manifest
    - datasets.ProductPicker.make_product_list
    
    And these functions now take the upload bucket as an argument, instead
    of using settings.base.TEST_CORPUS_BUCKET directly or indirectly:
    
    - CorpusFinalizer.upload_to_s3
    - CorpusFinalizer.create_and_upload_test_subset
    
    And *these* functions *do* look at MANIFEST_DIR or at
    TEST_CORPUS_BUCKET, as the default for a command line option:
    
    - ix_interface.sort
    - ix_interface.finalize
    
    notebooks/Coverage_Manifest_construction.ipynb is also updated for the
    modified API of coverage_utilz.  (This notebook is slated to be
    converted to an ix subcommand Real Soon.)

--- pdr_tests/ix_interface.py
+++ pdr_tests/ix_interface.py
@@ -12,6 +12,7 @@ from pdr_tests.datasets import (
     MissingHashError,
 )
 from pdr_tests.definitions import RULES_MODULES
+from pdr_tests.settings.base import MANIFEST_DIR, TEST_CORPUS_BUCKET
 from pdr_tests.utilz.ix_utilz import console_and_log
 
 
@@ -29,9 +30,13 @@ COMMANDS = [
 ]
 
 
-def sort(dataset, product_type=None):
+def sort(dataset, product_type=None, manifest_dir=None):
+    if manifest_dir is not None:
+        manifest_dir = Path(manifest_dir)
+    else:
+        manifest_dir = MANIFEST_DIR
     picker = ProductPicker(dataset)
-    picker.make_product_list(product_type)
+    picker.make_product_list(manifest_dir, product_type)
 
 
 def pick(
@@ -166,17 +171,28 @@ def test_paths(dataset=None, product_type=None):
     return tuple(chain.from_iterable(paths))
 
 
-def finalize(dataset=None, product_type=None, product=None, *,
+def finalize(dataset, product_type=None, product=None, *,
              regen: "r" = False,
              local: "l" = False,
-             subset_size: "n" = 1):
+             subset_size: "n" = 1,
+             bucket: "b" = None):
     """Creates a test subset (if necessary) and uploads relevant test files to s3."""
-    if dataset is None:
-        print("Upload requires a dataset argument. We don't want to re-upload all the files in the s3 bucket.")
-        return
-    else:
-        finalizer = CorpusFinalizer(dataset)
-        finalizer.create_and_upload_test_subset(product_type, product, subset_size, regen, local)
+    if bucket is None:
+        bucket = TEST_CORPUS_BUCKET
+    if not dataset:
+         raise ValueError(
+             "finalize requires a dataset argument."
+         )
+
+    finalizer = CorpusFinalizer(dataset)
+    finalizer.create_and_upload_test_subset(
+        product_type,
+        product,
+        subset_size,
+        regen,
+        local,
+        bucket,
+    )
 
 
 def index_directory(target, manifest, output="index.csv", debug=False, filters=None):
